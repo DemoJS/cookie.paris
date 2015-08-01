84a9802fb901f26a4598c3d99b3259e3f80fa2e2
Remi Papillie <remi.papillie@gmail.com>
2015-08-01 05:24:45 +0200

    basic registration system

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-var http = require("http")
-var url = require("url")
+var express = require("express")
+var app = express()
+var bodyParser = require('body-parser')
 
-var server = http.createServer(function(request, response)
+var redis = require("redis").createClient()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get("/api/subscribers", function(request, response)
 {
-	var resourcePath = url.parse(request.url).pathname
-	
-	response.writeHead(200)
-	response.write("cookie!")
-	response.end()
+    redis.lrange("cookie:subscribers", 0, -1, function(err, data)
+    {
+        if (err) return response.sendStatus(500)
+        
+        response.json(data)
+    })
 })
 
-server.listen(8092)
+app.post("/api/subscribers", function(request, response)
+{
+    var email = request.body.email
+    if ((typeof email !== "string") || (email.length === 0))
+        return response.sendStatus(400)
+    
+    email = email.substr(0, 100)
+    
+    redis.rpush("cookie:subscribers", email, function(err)
+    {
+        if (err) return response.sendStatus(500)
+        
+        response.send("Thanks for registering!")
+    })
+})
+
+app.use(express.static(__dirname + "/public"))
+
+app.use(function(err, request, response, next)
+{
+    console.log(err)
+    response.sendStatus(500)
+})
+
+app.listen(8092)
