159180d966dab6f01fb3f286e8b6d085694bb7f6
Noëlie <Nolie>
2016-11-19 03:38:56 +0100

    timetable, specs

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.post("/api/subscribers", function(request, response)
 app.get("/", function(request, response) { response.render("index") })
 app.get("/location", function(request, response) { response.render("location") })
 app.get("/competitions", function(request, response) { response.render("competitions") })
+app.get("/timetable", function(request, response) { response.render("timetable") })
 
 app.use(express.static(__dirname + "/public"))
 
