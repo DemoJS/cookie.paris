1a13eebc765026a94988225a8f90978e1675c449
Jonathan Giroux <giroux.jo@gmail.com>
2017-10-12 00:58:08 +0200

    Add visitors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser')
 
 var redis = require("redis").createClient()
 
+var sanitizeHtml = require('sanitize-html');
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -15,24 +17,34 @@ app.set("views", __dirname + "/views")
     redis.lrange("cookie:subscribers", 0, -1, function(err, data)
     {
         if (err) return response.sendStatus(500)
-        
+
         response.json(data)
     })
 })*/
 
-app.post("/api/subscribers", function(request, response)
+app.post("/api/register", function(request, response)
 {
-    var email = request.body.email
-    if ((typeof email !== "string") || (email.length === 0))
+    var handle = request.body.handle
+    if ((typeof handle !== "string") || (handle.length === 0))
         return response.sendStatus(400)
-    
-    email = email.substr(0, 100)
-    
-    redis.rpush("cookie:subscribers", email, function(err)
+    handle = handle.substr(0, 100)
+
+    var group = request.body.group
+    if ((typeof group !== "string"))
+        return response.sendStatus(400)
+    group = group.substr(0, 100)
+
+    var visitor = handle;
+    if (group)
+        visitor += " / " + group;
+
+    visitor = sanitizeHtml(visitor)
+
+    redis.rpush("cookie2017:visitors", visitor, function(err)
     {
         if (err) return response.sendStatus(500)
-        
-        response.send("Thanks for registering!")
+
+        response.redirect("/visitors")
     })
 })
 
@@ -40,6 +52,17 @@ app.get("/", function(request, response) { response.render("index") })
 app.get("/location", function(request, response) { response.render("location") })
 app.get("/competitions", function(request, response) { response.render("competitions") })
 app.get("/timetable", function(request, response) { response.render("timetable") })
+app.get("/visitors", function(request, response)
+{
+    return redis.lrange("cookie2017:visitors", 0, -1, function(err, visitors)
+    {
+        if (err) return response.sendStatus(500)
+
+        response.render("visitors", {
+            visitors: visitors,
+        })
+    })
+})
 
 app.use(express.static(__dirname + "/public"))
 
