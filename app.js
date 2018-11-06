0c9895df3ff986aaf1c6a91e251b99c1c0446b7d
Jonathan Giroux <giroux.jo@gmail.com>
2018-11-07 00:58:21 +0100

    Fix redis connection

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var app = express()
 var bodyParser = require('body-parser')
 var fs = require('fs')
 
-var redis = require("redis").createClient(6379, "192.168.99.100")
+var redis = require("redis").createClient()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 app.set("view engine", "jade")
 app.set("views", __dirname + "/views")
@@ -21,7 +23,7 @@ app.set("views", __dirname + "/views")
     })
 })*/
 
-app.post("/api/register", function(request, response) {
+app.post("/api/register", function (request, response) {
     var handle = request.body.handle
     if ((typeof handle !== "string") || (handle.length === 0))
         return response.sendStatus(400)
@@ -36,16 +38,18 @@ app.post("/api/register", function(request, response) {
     if (group)
         visitor += " / " + group;
 
-    redis.rpush("cookie2018:visitors", visitor, function(err) {
+    redis.rpush("cookie2018:visitors", visitor, function (err) {
         if (err) return response.sendStatus(500)
 
         response.redirect("/visitors")
     })
 })
 
-app.get("/", function(request, response) { response.render("index") })
-app.get("/visitors", function(request, response) {
-    return redis.lrange("cookie2018:visitors", 0, -1, function(err, visitors) {
+app.get("/", function (request, response) {
+    response.render("index")
+})
+app.get("/visitors", function (request, response) {
+    return redis.lrange("cookie2018:visitors", 0, -1, function (err, visitors) {
         if (err) return response.sendStatus(500)
 
         response.render("visitors", {
@@ -54,11 +58,11 @@ app.get("/visitors", function(request, response) {
     })
 })
 
-app.get("/:page", function(request, response, next) {
+app.get("/:page", function (request, response, next) {
     fs.access('views/' + request.params.page + '.jade', fs.constants.R_OK, (err) => {
         if (err) return next()
 
-        response.render(request.params.page, function(err, html) {
+        response.render(request.params.page, function (err, html) {
             if (err) return response.sendStatus(404)
 
             response.send(html)
@@ -68,7 +72,7 @@ app.get("/:page", function(request, response, next) {
 
 app.use(express.static(__dirname + "/public"))
 
-app.use(function(err, request, response, next) {
+app.use(function (err, request, response, next) {
     console.log(err)
     response.sendStatus(500)
 })
